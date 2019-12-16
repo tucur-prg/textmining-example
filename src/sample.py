5211be401b6488c6a704dde148fc5b286aa56764
tucur-prg <tucur.prg+github@gmail.com>
2019-12-16 15:55:53 +0900

    use FILE as resource file

--- src/sample.py
+++ src/sample.py
@@ -1,7 +1,8 @@
 import MeCab
 
-mecab = MeCab.Tagger('-u /app/user.dic')
+#mecab = MeCab.Tagger('-u /app/user.dic')
 #mecab = MeCab.Tagger('-Ochasen') #デフォルト辞書を使う場合
+mecab = MeCab.Tagger('-r /app/mecabrc')
 
 text = '「ナチュラルハイ20周年記念作品 接客中に顔を紅潮させながら感じまくるバイト娘 真夏の看板娘中出しSP 〜海の家、ガソリンスタンド、祭りの的屋、ビアガーデン〜 総勢27人総集編付き2枚組480分 超豪華版」を動画配信で購入しましたが、ストリーミングで前半の二人分（約2時間40分）しかなく、後半の二人分及び総集編を見ることができません。ご確認お願いします。'
 
