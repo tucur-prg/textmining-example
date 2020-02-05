181c22dee2397fcb2a2ac1933569a6aa081e87d6
tucur-prg <tucur.prg+github@gmail.com>
2020-02-05 17:10:49 +0900

    GiNZAのサンプル追加、MeCabのサンプルリネーム

---
+++ src/example_ginza.py
@@ -0,0 +1,22 @@
+import spacy
+
+nlp = spacy.load('ja_ginza')
+
+text = '「ナチュラルハイ20周年記念作品 接客中に顔を紅潮させながら感じまくるバイト娘 真夏の看板娘中出しSP 〜海の家、ガソリンスタンド、祭りの的屋、ビアガーデン〜 総勢27人総集編付き2枚組480分 超豪華版」を動画配信で購入しましたが、ストリーミングで前半の二人分（約2時間40分）しかなく、後半の二人分及び総集編を見ることができません。ご確認お願いします。'
+
+doc = nlp(text)
+
+for sent in doc.sents:
+    print(sent)
+
+    for token in sent:
+        info = [
+            token.i,         # トークン番号
+            token.orth_,     # テキスト
+            token._.reading, # 読みカナ
+            token.lemma_,    # 基本形
+            token.pos_,      # 品詞
+            token.tag_,      # 品詞詳細
+            token._.inf      # 活用情報
+        ]
+        print(info)
